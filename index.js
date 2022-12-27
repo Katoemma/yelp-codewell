a65c9b34aebf4de84422d979d64a1834f3c71307
Katoemma <103442484+Katoemma@users.noreply.github.com>
2022-12-27 21:54:39 +0300

    modified the promo js

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 var promi = document.getElementById("prom");
+var navs = document.getElementById("navigation");
 function promo(elem) {
     promi.style.display = "none";
+}
+var btns = document.getElementsByClassName("nav-btns");
+function open(x) {
+    btns.style.display = "block";
 }
